4bdb545d40c1b4939f3a4637f09e410d86c529b0
Lucas <lucas@github.com>
2018-10-29 17:02:34 -0300

    Fazendo mais alterações no view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,46 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class Controller {
 
+    double x,y;
+    Stage stage = null;
+    @FXML
+    void dragedMenu(MouseEvent event) {
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void pressedMenu(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+    @FXML
+    void min(MouseEvent event) {
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    void close(MouseEvent event) {
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
     public void closeIcon(ActionEvent event) {
         Platform.exit();
         System.exit(0);
