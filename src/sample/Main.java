f7463b0b0da8b0a4d79bf84fee10e456c69e57fd
Lucas <lucas@github.com>
2018-10-29 17:24:00 -0300

    Fazendo mais alterações no view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
         primaryStage.initStyle(StageStyle.UNDECORATED);
         //primaryStage.setResizable(false);
         primaryStage.show();
+        ///
 
     }
 
