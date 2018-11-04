c5de0dc377ef5f8b05764f258516153648582895
Lucas <Lucas@github.com>
2018-11-04 03:15:22 -0200

    Terminado a parte de reserva.

--- src/sample/Reserva.java
+++ src/sample/Reserva.java
@@ -2,6 +2,8 @@ package sample;
 
 import Banco.Conexao;
 import Model.ModelReserva;
+import de.jensd.fx.glyphs.materialicons.MaterialIconView;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +12,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -22,9 +25,11 @@ import java.util.ResourceBundle;
 public class Reserva implements Initializable {
     private ModelReserva mReserva;
     Integer qtdDisponivel;
+    ObservableList<Node> childrens;
     @FXML
     private Text l_Orig;
-
+    @FXML
+    private GridPane GP_lugares;
     @FXML
     private Text l_Dest;
 
@@ -58,11 +63,37 @@ public class Reserva implements Initializable {
     }
 
     @FXML
+    void ProxScene(ActionEvent event) {
+        try {
+            Conexao conexao = new Conexao();
+            int posicao;
+            ResultSet rs = null;
+            Statement statement = null;
+            conexao.connect();
+            statement = conexao.createStatement();
+            if (qtdDisponivel == 0) {
+                childrens = GP_lugares.getChildren();
+                for (Node node : childrens) {
+                    if (node.getStyle().equals("-fx-fill: #a11616; -fx-cursor: Hand;")) {
+                        posicao = childrens.indexOf(node);
+                        System.out.println(posicao);
+                        statement.executeUpdate("insert into assento(id,sequencia,id_voo,num_Assento) values(1,(select MAX(sequencia) from assento where id = '1')+1,1,"+posicao+")");
+                    }
+                }
+                statement.close();
+            } else {
+                JOptionPane.showMessageDialog(null, "Quantidade maxima não atiginda.");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @FXML
     void chairClick(MouseEvent event) {
 
             // lugar já reservado.
             if (((Node) event.getSource()).getStyle().equals("-fx-fill: #949191;")) {
-                JOptionPane.showMessageDialog(null, "Lugar já Reservado!1");
+                JOptionPane.showMessageDialog(null, "Lugar já Reservado!");
             } else {
                 if (((Node) event.getSource()).getStyle().equals("-fx-fill: #a11616; -fx-cursor: Hand;")) {
                     ((Node) event.getSource()).setStyle("-fx-fill: #1fe05c; -fx-cursor: Hand;");
@@ -85,6 +116,9 @@ public class Reserva implements Initializable {
         l_Dest.setText(value.getDestino());
         qtdDisponivel = value.getQtdAdulto()+ value.getQtdCrianca();
         l_qtdDis.setText(Integer.toString(qtdDisponivel));
+        childrens = GP_lugares.getChildren();
+        MaterialIconView miv = null;
+        int posicao = 0;
         try{
             if(this.mReserva != null){
                 Conexao conexao = new Conexao();
@@ -94,9 +128,17 @@ public class Reserva implements Initializable {
                 statement = conexao.createStatement();
                 rs = statement.executeQuery("select t2.Nome as Origem,t3.Nome as Destino,t4.num_Assento as Assento,t1.DataPartida from voo  t1 inner join Paises  t2 on t1.id_POrigem = t2.id inner join Paises  t3 on t1.id_PDestino = t3.id inner join Assento t4 on t1.id = t4.id_Voo where Origem = \'"+l_Orig.getText()+"\' and Destino = \'"+l_Dest.getText()+"\' and DataPartida = \'"+mReserva.getIda()+"\'");
                 while(rs.next()){
-                    //implementar logica para deixar os assentos cinzas.
+                    miv = (MaterialIconView) childrens.get(rs.getInt("Assento"));
+                    miv.setStyle("-fx-fill: #949191;");
+//                    for(Node node: childrens){
+//                        if(posicao==rs.getInt("Assento")){
+//                            System.out.println("s");
+//                        }
+//                        posicao++;
+//                    }
                 }
                 System.out.println("Teste");
+                statement.close();
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
