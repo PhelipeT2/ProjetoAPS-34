e41a6dbbfad2cb31eecf8e2d9fb674f998963463
financeiro <anny_cmgoncalves@homail.com>
2018-11-07 17:39:09 -0200

    Tela com dois dependentes para teste e edição tela pagamento para igualar

--- src/Model/ModelDependentes.java
+++ src/Model/ModelDependentes.java
@@ -3,52 +3,33 @@ package Model;
 import java.util.Date;
 
 public class ModelDependentes {
-
-    private String DepName;
-    private String DepSName;
+    private String DepName, DepDoc;
     private Date DepAge;
-    private String DepDoc;
 
-    public ModelDependentes(String depName, String depSName, Date depAge, String depDoc) {
+    private String DepName2, DepDoc2;
+    private Date DepAge2;
+
+    public ModelDependentes(String depName, Date depAge, String depDoc,
+                            String depName2, Date depAge2, String depDoc2) {
         DepName = depName;
-        DepSName = depSName;
         DepAge = depAge;
         DepDoc = depDoc;
+
+        DepName2 = depName2;
+        DepAge2 = depAge2;
+        DepDoc2 = depDoc2;
     }
-    public String getDepName() {
-        return DepName;
-    }
+    public String getDepName() { return DepName; }
+    public void setDepName(String depName) { DepName = depName; }
+    public Date getDepAge() { return DepAge; }
+    public void setDepAge(Date depAge) { DepAge = depAge; }
+    public String getDepDoc() { return DepDoc; }
+    public void setDepDoc(String depDoc) { DepDoc = depDoc; }
 
-    public void setDepName(String depName) {
-        DepName = depName;
-    }
-
-    public String getDepSName() {
-        return DepSName;
-    }
-
-    public void setDepSName(String depSName) {
-        DepSName = depSName;
-    }
-
-    public Date getDepAge() {
-        return DepAge;
-    }
-
-    public void setDepAge(Date depAge) {
-        DepAge = depAge;
-    }
-
-    public String getDepDoc() {
-        return DepDoc;
-    }
-
-    public void setDepDoc(String depDoc) {
-        DepDoc = depDoc;
-    }
-
-
-
-
-
+    public String getDepName2() { return DepName2; }
+    public void setDepName2(String depName2) { DepName2 = depName2; }
+    public Date getDepAge2() { return DepAge2; }
+    public void setDepAge2(Date depAge2) { DepAge2 = depAge2; }
+    public String getDepDoc2() { return DepDoc2; }
+    public void setDepDoc2(String depDoc2) { DepDoc2 = depDoc2; }
 }
